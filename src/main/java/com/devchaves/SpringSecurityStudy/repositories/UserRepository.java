8aadca3fd67bdb196249c83f20168e330a0ab72c
chaves <chavesprogrammer@gmail.com>
2025-03-11 11:17:55 -0300

    add userRepo

--- src/main/java/com/devchaves/SpringSecurityStudy/repositories/UserRepository.java
+++ src/main/java/com/devchaves/SpringSecurityStudy/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package com.devchaves.SpringSecurityStudy.repositories;
 
-public interface UserRepository {
+import com.devchaves.SpringSecurityStudy.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
 }
