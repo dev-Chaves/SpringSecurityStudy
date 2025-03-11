184814a1c75fe559dd969cb09570f3a980715336
chaves <chavesprogrammer@gmail.com>
2025-03-11 11:17:35 -0300

    add TweetRepo

--- src/main/java/com/devchaves/SpringSecurityStudy/repositories/TweetRepository.java
+++ src/main/java/com/devchaves/SpringSecurityStudy/repositories/TweetRepository.java
@@ -1,4 +1,9 @@
 package com.devchaves.SpringSecurityStudy.repositories;
 
-public interface TweetRepository {
+import com.devchaves.SpringSecurityStudy.model.TweetModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends JpaRepository<TweetModel, Long> {
 }
